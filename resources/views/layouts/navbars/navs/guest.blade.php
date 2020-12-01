7505a5af9919473f5f73577d28489f377981c8b4
MartonPadilha <outroswp@gmail.com>
2020-11-30 21:24:10 -0300

    Edit page Login

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -4,12 +4,12 @@
     <div class="navbar-wrapper">
       <a class="navbar-brand" href="{{ route('home') }}">{{ $activePage }}</a>
     </div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+    {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
       <span class="sr-only">Toggle navigation</span>
       <span class="navbar-toggler-icon icon-bar"></span>
       <span class="navbar-toggler-icon icon-bar"></span>
       <span class="navbar-toggler-icon icon-bar"></span>
-    </button>
+    </button> --}}
     <div class="collapse navbar-collapse justify-content-end">
       <ul class="navbar-nav">
         {{-- <li class="nav-item">
